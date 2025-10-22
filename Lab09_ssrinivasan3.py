0a6bc4f3cbe380fb366d5f6e9129529aa82c53d0
Shrrayashwinz <sshrrayash@gmail.com>
2025-10-22 11:07:32 -0400

    started the class

--- Lab09_ssrinivasan3.py
+++ Lab09_ssrinivasan3.py
@@ -7,6 +7,17 @@ Description: Create a coin toss game with two players. Each player starts with 2
 Dont forget classes. Players either win or lose based on coin tosses.
 """
 
+import random
+class Coin:
+    def __init__(self):
+      self.__amount = 20
+      self.__sideup = 'Heads' if random.randint(0, 1) == 0 else 'Tails'
+    
+    def toss(self):
+
+
+    
+
 def main ():
     print("Welcome to the coin toss game! Each player has 20 coins.")
     player_1 = coin()
