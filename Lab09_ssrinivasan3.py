a501f3cee00e433578c637efa021668a1831b30d
Shrrayashwinz <sshrrayash@gmail.com>
2025-10-22 09:58:43 -0400

    Made some edits

--- Lab09_ssrinivasan3.py
+++ Lab09_ssrinivasan3.py
@@ -4,5 +4,10 @@ Name: Shrrayash Srinivasan
 Date: October 21, 2025
 
 Description: Create a coin toss game with two players. Each player starts with 20 coins. 
-Dont forget classes.
+Dont forget classes. Players either win or lose based on coin tosses.
 """
+
+def main ():
+    print("Welcome to the coin toss game! Each player has 20 coins.")
+    player_1 = coin()
+    player_2 = coin()
