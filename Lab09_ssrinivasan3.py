cc37e4aabe22ad30f3ee9a47bf989fcc27849720
Shrrayashwinz <sshrrayash@gmail.com>
2025-10-23 10:29:45 -0400

    Added some more code. Completed the coin classes include the init, toss, amount nad sideup methods.

--- Lab09_ssrinivasan3.py
+++ Lab09_ssrinivasan3.py
@@ -12,3 +12,14 @@ def main ():
     print("Welcome to the coin toss game! Each player has 20 coins.")
     player_1 = coin()
     player_2 = coin()
+    play = input("Do you wish to play: Y / N.")
+
+    while play == 'y':
+      player_1.toss()
+      player_2.toss()
+
+      side_1 = player_1.get_sideup()
+      side_2 = player_2.get_sideup()
+
+
+
--- coin.py
+++ coin.py
@@ -1,7 +1,48 @@
+"""
+Program Name: coin.py
+Author: Shrrayash Srinivasan
+Purpose: Defines the Coin class for the Match Coins game, including toss logic and coin tracking.
+Date: October 23, 2025
+"""
+
 import random
 class Coin:
     def __init__(self):
-      self.__amount = 20
-      self.__sideup = 'Heads' if random.randint(0, 1) == 0 else 'Tails'
+        self.__amount = 20
+        self.__sideup = random.choice(['Heads', 'Tails'])
+        self.__spend = "Slap ur head!"
     
     def toss(self):
+        self.__sideup = 'Heads' if random.randit(0,1) == 0 else 'Tails'
+    
+    def get_sideup(self):
+        return self.__sideup
+    
+    def set_amount(self, change):
+        self.__amount += change
+    
+    def get_amount(self):
+        return self.__amount
+    
+    def spend(self):
+        return self.__spend
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
+
+      
+
+
+
+
+
